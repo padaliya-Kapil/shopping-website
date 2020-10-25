82b6ce5625cd37ce240db08f7d9789a9dcc0c0f1
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-10-25 14:25:54 -0400

    Fetch products from db

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 
  
 // this is the file use product.js
-import products from './data/products.js'
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 const PORT = process.env.PORT || 5000
  
@@ -17,16 +18,6 @@ app.get('/',(req, res)=>{
     res.send('API is running')
 });
 
-app.get('/api/products',(req, res)=>{
-
-    res.json(products)
-
-});
-
-app.get('/api/products/:id',(req, res)=>{
-    const product = products.find((p)=> p._id === req.params.id)
-    res.json(product)
-});
-
+app.use('/api/products',productRoutes);
 
 app.listen(PORT,console.log(`Server running on port number ${PORT} in ${process.env.NODE_ENV} environment.`))
