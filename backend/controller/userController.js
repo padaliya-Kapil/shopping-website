6ec95268aac0f8e74dd0a99e57e270b402ad41da
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-10-30 01:15:01 -0400

    token fetching data

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -26,4 +26,27 @@ const authUsers = asyncHandler ( async (req,res) => {
     }
 })
 
-export {authUsers}
+
+// @desc Get user profile
+// @route POSY /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler ( async (req,res) => {
+    
+    const user = await User.findById(req.user.id)
+
+    if(user){
+        res.json({
+            _id : user.id,
+                name : user.name,
+                email : user.email,
+                isAdmin : user.isAdmin
+        })
+        
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+   
+})
+
+export {authUsers , getUserProfile}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
-import {authUsers} from '../controller/userController.js'
+import {authUsers,getUserProfile} from '../controller/userController.js'
+import {protect} from '../middleware/authMiddleware.js'
 
 import express from 'express'
 const router = express.Router()
 
 router.post('/login',authUsers);
+router.route('/profile')
+        .get(protect,getUserProfile);  // protect is the middleware
 
 
 export default router
