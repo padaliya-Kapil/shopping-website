e305d323f0287647493bcd72c263949349d795ce
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-10-30 01:45:50 -0400

    backend user complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
-import {authUsers,getUserProfile} from '../controller/userController.js'
+import {authUsers,getUserProfile , registerUsers} from '../controller/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 import express from 'express'
 const router = express.Router()
 
+router.route('/').post(registerUsers);
 router.post('/login',authUsers);
 router.route('/profile')
         .get(protect,getUserProfile);  // protect is the middleware
