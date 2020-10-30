ae88db5bd07b71958aa05df18d7a3f839ab6b0d3
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-10-30 19:44:43 -0400

    update user endpoint

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -83,4 +83,38 @@ const getUserProfile = asyncHandler ( async (req,res) => {
    
 })
 
-export {authUsers , getUserProfile , registerUsers}
+
+// @desc Update details of an existing user
+// @route PUT  /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler ( async (req,res) => {
+  
+    const user = await User.findById(req.user.id)
+
+    if(user){
+        
+                user.name = req.body.name || user.name,
+                user.email = req.body.email || user.email 
+
+          if(req.body.password){
+              user.password = req.body.password
+          }      
+
+          const updatedUser = await user.save()
+
+          res.json({
+            _id : updatedUser.id,
+            name : updatedUser.name,
+            email : updatedUser.email,
+            isAdmin : updatedUser.isAdmin,
+            token : generateToken(updatedUser._id) 
+        })
+        
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
+export {authUsers , getUserProfile , registerUsers, updateUserProfile} 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,8 @@
-import {authUsers,getUserProfile , registerUsers} from '../controller/userController.js'
+import {authUsers,
+        getUserProfile ,
+         registerUsers,
+         updateUserProfile
+        } from '../controller/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 import express from 'express'
@@ -7,7 +11,7 @@ const router = express.Router()
 router.route('/').post(registerUsers);
 router.post('/login',authUsers);
 router.route('/profile')
-        .get(protect,getUserProfile);  // protect is the middleware
-
+        .get(protect,getUserProfile)  // protect is the middleware
+        .put(protect,updateUserProfile)
 
 export default router
