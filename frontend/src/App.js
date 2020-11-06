c716a94ac34a97e4ec266f7a816571892fe61e86
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-11-05 23:23:46 -0500

    productScreenForntend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 
 
@@ -40,6 +41,7 @@ const App=() => {
         <Route path ='/order/:id' component={OrderScreen}/>
         <Route path ='/admin/userlist' component={UserListScreen}/>
         <Route path ='/admin/user/:id/edit' component={UserEditScreen}/>
+        <Route path = '/admin/productlist' component = {ProductListScreen}/>
 
        
 
