fc7548e7176837e782b6b998d618c4c89a71bad0
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-11-09 19:43:48 -0500

    build done for deployment

---
+++ frontend/build/precache-manifest.812f8ff1f2ad43ed8b225dbb07a8200f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97df3adbcdf11c0da2469c3c94040b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d7a67674f82c1a5508",
+    "url": "/static/css/main.8950e375.chunk.css"
+  },
+  {
+    "revision": "36c89b536b163ac55bc6",
+    "url": "/static/js/2.2883e8ac.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.2883e8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d7a67674f82c1a5508",
+    "url": "/static/js/main.11f32bd5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
